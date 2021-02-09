0516db7a4494d3ffaf435935d9860d5454942597
laurence allen <you@example.com>
2021-02-09 10:57:58 +0000

    New code for finding x_prime and y_prime

--- line-plane intersection.py
+++ line-plane intersection.py
@@ -12,3 +12,109 @@ def line_plane_angle(x, x_0, y, y_0, z, z_0): #(x,y,z) coords are the plane of t
     return theta
 
 print(line_plane_angle(1, 2, 2, 3, 0, -5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps):
+    a = np.tan(alpha)
+    
+    x_prime_vals = []
+    y_prime_vals = []
+    
+    for i in range(0,2*np.pi, steps): #i is our psi variable
+        
+        z = z_prime/(a*np.cos(i)*np.cos(theta)*np.cos(phi)
+            - a*np.sin(i)*np.sin(phi) + np.sin(theta)*np.cos(phi))
+
+        y_prime = z*(a*np.cos(i)*np.cos(theta)*np.sin(phi)
+            + a*np.sin(i)*np.cos(phi) + np.sin(theta)*np.sin(phi))
+
+        x_prime = z*(a*np.cos(i)*np.sin(theta) + np.cos(theta)) 
+        
+        y_prime_vals.append(y_prime)
+    
+        x_prime_vals.append(x_prime)
+    
+    return x_prime_vals, y_prime_vals
