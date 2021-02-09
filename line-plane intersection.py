5a66082bb6d3e5fd3c6a583aca6af01324659160
Lawrence McNeillis <lawrence241@hotmail.co.uk>
2021-02-09 12:13:15 +0000

    corrections and prime notation

--- line-plane intersection.py
+++ line-plane intersection.py
@@ -7,15 +7,15 @@ Created on Fri Feb  5 15:09:55 2021
 
 import numpy as np
 
-def theta_angle(x, x_0, y, y_0, z, z_0):
+def theta_angle(x_prime, x_0_prime, y_prime, y_0_prime, z_prime, z_0_prime):
     '''
     Calculate the angle between the cone axial vector and the z_prime axis
 
     Parameters
     ----------
-    x,y,z : TYPE - float
+    x_prime, y_prime, z_prime : TYPE - float
         DESCRIPTION - x,y,z coordinate of the hit on the scattering detector in primed (global) coordinate system. z=0 by definition.
-    x_0, y_0, z_0 : TYPE - float
+    x_0__prime, y_0__prime, z_0__prime : TYPE - float
         DESCRIPTION - x_0, y_0, z_0 coordinate of the hit on the absorbing detector in primed (global) coordinate system.
 
     Returns
@@ -24,16 +24,16 @@ def theta_angle(x, x_0, y, y_0, z, z_0):
         DESCRIPTION.
 
     '''
-    theta = np.arccos((z- z_0)/np.sqrt((x-x_0)**2 + (y-y_0)**2 + (z-z_0)**2))
+    theta = np.arccos((z_prime - z_0_prime)/np.sqrt((x_prime - x_0_prime)**2 + (y_prime - y_0_prime)**2 + (z_prime - z_0_prime)**2))
     return theta
 
-print(theta_angle(1, 2, 2, 3, 0, -5))
 
-def cone_vector(x, x_0, y, y_0, z, z_0):
+
+def cone_vector(x_prime, x_0_prime, y_prime, y_0_prime, z_prime, z_0_prime):
     '''
     Returns cone axis vector as a list in primed axes.
     '''
-    return [(x-x_0), (y-y_0), (z-z_0)]
+    return [(x_prime-x_0_prime), (y_prime-y_0_prime), (z_prime-z_0_prime)]
     
 def N(z):
     '''
@@ -54,7 +54,7 @@ def N(z):
 
     '''
     
-    return [-z[0], z[1], 0]
+    return [-z[1], z[0], 0]
 
 def phi_angle(N):
     '''
@@ -72,7 +72,7 @@ def phi_angle(N):
 
     '''
     
-    return np.arccos(N[0]/(N[0]**2 + N[1]**2))
+    return np.arccos(N[0]/((N[0]**2 + N[1]**2)**0.5))
 
 def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps):
     a = np.tan(alpha)
@@ -82,13 +82,13 @@ def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps):
     
     for i in range(0,2*np.pi, steps): #i is our psi variable
         
-        z = z_prime/(a*np.cos(i)*np.cos(theta)*np.cos(phi)
-            - a*np.sin(i)*np.sin(phi) + np.sin(theta)*np.cos(phi))
+        z = z_prime/(-a*np.cos(i)*np.sin(theta) + np.cos(theta))
 
         y_prime = z*(a*np.cos(i)*np.cos(theta)*np.sin(phi)
             + a*np.sin(i)*np.cos(phi) + np.sin(theta)*np.sin(phi))
 
-        x_prime = z*(a*np.cos(i)*np.sin(theta) + np.cos(theta)) 
+        x_prime = z*(a*np.cos(i)*np.cos(phi)*np.cos(theta) - a*np.sin(i)*np.sin(phi) + 
+                     np.cos(phi)*np.sin(theta)) 
         
         y_prime_vals.append(y_prime)
     
