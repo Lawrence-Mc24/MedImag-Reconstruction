26c412246583de3d841104fe23ced90a48799e1a
Adamg1711 <axg845@student.bham.ac.uk>
2021-02-09 10:28:34 +0000

    Added function for theta

--- test_file.py
+++ test_file.py
@@ -20,8 +20,9 @@ m_e = scipy.constants.m_e
 c = scipy.constants.c
 e = scipy.constants.e
 
-#Function calculating scatter angle from initial and final energy (Joules)
 def compton_angle(E_initial, E_final):
+    '''Function calculating Compton scatter angle from initial and final
+    energy (Joules)'''
     E_initial = E_initial*e
     E_final = E_final*e
     initial = h*c/E_initial
@@ -29,8 +30,18 @@ def compton_angle(E_initial, E_final):
     angle = np.arccos(1 - (m_e*c/h)*(final-initial))
     return angle
 
-#Calculating error on scatter angle
-def angle_err(err_initial, err_final):
+def compton_angle_err(err_initial, err_final):
+    '''Error on the Compton angle'''
     denom = np.sqrt(1-(1-m_e*c/h)**2)
     err_angle = np.sqrt((err_initial*denom)+(err_final*denom))
     return err_angle
+
+def theta(x, x0, y, y0, z, z0):
+    '''Finding the angle between the line of response and the normal to the
+    detector plane'''
+    diff_x = x-x0
+    diff_y = y-y0
+    diff_z = z-z0
+    denom = np.sqrt((diff_x)**2+(diff_y)**2+(diff_z)**2)
+    theta = np.arccos(diff_z*denom)
+    return theta
