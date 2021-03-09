71c397442be5307878f7e29ed880f9de8895f647
Lawrence McNeillis <lawrence241@hotmail.co.uk>
2021-03-09 11:21:37 +0000

    added ROI

--- reconstruction_code.py
+++ reconstruction_code.py
@@ -21,7 +21,9 @@ e = scipy.constants.e
 # path = 'D:/University/Year 3/Group Studies/Monte Carlo data/compt_photo_chain_data_4_detectors.csv'
 #path =  r'C:\Users\laure\Documents\Physics\Year 3\Group Study\Point_Source-Truth_Data_1.csv'
 #path = 'D:/University/Year 3/Group Studies/Monte Carlo data/compt_photo_chain_data_4_detectors.csv'
-path = "U:\Physics\Yr 3\MI Group Studies\MC data/compt_photo_chain_data_45_degrees_point_source.csv"
+#path = "U:\Physics\Yr 3\MI Group Studies\MC data/compt_photo_chain_data_45_degrees_point_source.csv"
+path = r"C:\Users\lawre\Documents\Y3_Compton_Camera\compt_photo_chain_data_.csv"
+
 dataframe = pd.read_csv(path)
 
 x_prime = dataframe['X_1 [cm]']
@@ -205,11 +207,11 @@ def psi_calculator(ds, theta, phi, z_prime, a, n, alpha):
             psi_list.append(psi)
     return psi_list
 
-def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps, r1, estimate, ds=0, ):
+def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps, r1, estimate, ROI, ds=0,):
     a = np.tan(alpha)
     # print(f'a is {a}')
     if alpha + theta > np.pi/2:
-        return x_prime_y_prime_parabola(z_prime, theta, phi, alpha, steps, r1, estimate, ds)
+        return x_prime_y_prime_parabola(z_prime, theta, phi, alpha, steps, r1, estimate, ROI, ds)
     x_prime_vals = np.array([])
     y_prime_vals = np.array([])
     ds=ds
@@ -228,13 +230,13 @@ def x_prime_y_prime_output(z_prime, theta, phi, alpha, steps, r1, estimate, ds=0
         y_prime = z*(a*np.cos(i)*np.cos(theta)*np.sin(phi)
             + a*np.sin(i)*np.cos(phi) + np.sin(theta)*np.sin(phi)) + r1[1]
 
-        
-        x_prime_vals = np.append(x_prime_vals, x_prime)
-        y_prime_vals = np.append(y_prime_vals, y_prime)
+        if ROI[0] < x_prime < ROI[1] and ROI[2] < y_prime < ROI[3]: 
+            x_prime_vals = np.append(x_prime_vals, x_prime)
+            y_prime_vals = np.append(y_prime_vals, y_prime)
 
     return x_prime_vals, y_prime_vals, ds
 
-def x_prime_y_prime_parabola(z_prime, theta, phi, alpha, steps, r1, estimate, ds):
+def x_prime_y_prime_parabola(z_prime, theta, phi, alpha, steps, r1, estimate, ROI, ds):
     a = np.tan(alpha)
     
     x_prime_vals = np.array([])
@@ -267,9 +269,11 @@ def x_prime_y_prime_parabola(z_prime, theta, phi, alpha, steps, r1, estimate, ds
         
         y_prime = z*(a*np.cos(psi)*np.cos(theta)*np.sin(phi)
             + a*np.sin(psi)*np.cos(phi) + np.sin(theta)*np.sin(phi)) + r1[1]
+        
+        if ROI[0] < x_prime < ROI[1] and ROI[2] < y_prime < ROI[3]: 
+            x_prime_vals = np.append(x_prime_vals, x_prime)
+            y_prime_vals = np.append(y_prime_vals, y_prime)
 
-        x_prime_vals = np.append(x_prime_vals, x_prime)
-        y_prime_vals = np.append(y_prime_vals, y_prime)
         
         d = dpsi(ds, theta, phi, psi, z_prime, a)
         if counter > 5000:
@@ -367,7 +371,7 @@ def plot_it(x, ys, r1, x_name='x', y_name='y', plot_title='Plot', individual_poi
     plt.show()
     return figure
 
-def give_x_y_for_two_points(r1, r2, z_prime, alpha, steps, estimate, ds):
+def give_x_y_for_two_points(r1, r2, z_prime, alpha, steps, estimate, ROI, ds):
     '''
     Computes an array of (x, y) points on the imaging plane a distance z_prime from the scatter detector
     for an event at points r1, r2.
@@ -389,7 +393,7 @@ def give_x_y_for_two_points(r1, r2, z_prime, alpha, steps, estimate, ds):
     theta = theta_angle(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])
     phi = phi_angle(N(cone_vector(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])))
     # print(f'theta = {theta}, phi = {phi}')
-    x, y, ds = x_prime_y_prime_output(z_prime, theta, phi, alpha, steps, r1, estimate, ds)
+    x, y, ds = x_prime_y_prime_output(z_prime, theta, phi, alpha, steps, r1, estimate, ROI, ds)    
     # print(x, y)
     return x, y, ds
 
@@ -527,7 +531,7 @@ def calculate_heatmap(x, y, bins=50, dilate_erode_iterations=5, ZoomOut=0):
     
     x_chop = xedges[chop_indices[0]+1], xedges[chop_indices[1]]
     y_chop = yedges[chop_indices[2]+1], yedges[chop_indices[3]]
-    bins2 = 100
+    bins2 = 50
     print(f'y_bins = {y_bins}', f', x_bins = {bins}')
     heatmaps2 = []
     for i in range(len(x)):
@@ -574,7 +578,7 @@ def image_slicer(h, ZoomOut=0):
         
     return chop_indices, ind
 
-def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps=180, plot=True, ZoomOut=0):
+def get_image(points, n, estimate, image_distance, source_energy, bins, R, ROI, steps=180, plot=True, ZoomOut=0):
     '''
     Parameters
     ----------
@@ -593,6 +597,8 @@ def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps
         DESCRIPTION - number of bins to construct heatmap
     R : TYPE - float
         DESCRIPTION - resolution of the detector
+    ROI : TYPE - array
+        DESCRIPTION - region of interest to image of the form [xmin, xmax, ymin, ymax]
     steps : TYPE - integer
         DESCRIPTION - approximate number of steps to take in psi to calculate cone projections 
     plot : TYPE - boolean
@@ -604,7 +610,7 @@ def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps
         y along axis 1.
 
     '''
-    n_points = 390
+    n_points = 2000
     if n_points > np.shape(points)[0]:
         n_points = np.shape(points)[0]
             
@@ -644,7 +650,7 @@ def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps
             for angle in alpha_bounds:
                 # print(f'r1={r1}')
                 # print(f'r2={r2}')
-                x, y, ds = give_x_y_for_two_points(r1, r2 , z_prime=image_distance, alpha=angle, steps=steps, estimate=estimate, ds=ds)
+                x, y, ds = give_x_y_for_two_points(r1, r2 , image_distance, angle, steps, estimate, ROI, ds=ds)
                 xs2 = np.append(xs2, x, axis=0)
                 ys2 = np.append(ys2, y, axis=0)
                 x_list.append(xs2)
@@ -652,7 +658,7 @@ def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps
         else:
             # print(f'r1={r1}')
             # print(f'r2={r2}')
-            x, y, ds = give_x_y_for_two_points(r1, r2 , z_prime=image_distance, alpha=alpha, steps=steps, estimate=estimate, ds=ds)
+            x, y, ds = give_x_y_for_two_points(r1, r2 , image_distance, alpha, steps, estimate, ROI, ds=ds)
             xs2 = np.append(xs2, x, axis=0)
             ys2 = np.append(ys2, y, axis=0)
             x_list.append(xs2)
@@ -667,7 +673,37 @@ def get_image(points, n, estimate, image_distance, source_energy, bins, R, steps
 
     if plot is True:
         plot_heatmap(heatmap_combined, extent_combined, bins, y_bins, n_points)
-    
+    print(j)
     return heatmap_combined, extent_combined
 
-heatmap, extent = get_image(points, 50, 30, 30, 662E3, 25, R=0, steps=50, ZoomOut=0)
+# r1 = np.array([-0.4, 0.5, 0])
+# r2 = np.array([0.05, 0.1, -1])
+# r3 = np.array([0, 0, 0])
+# r4 = np.array([0.05, 0.1, -1])
+# r5 = np.array([-0.3, 0.4, 0])
+# r6 = np.array([0.05, 0.1, -1])
+# xys = []
+# points = np.array([[r1, r2], [r3, r4], [r5, r6]])
+# for point in points:
+#     r1 = point[0]
+#     r2 = point[1]
+#     theta = theta_angle(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])
+#     phi = phi_angle(N(cone_vector(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])))
+#     x, y, ds = x_prime_y_prime_output(1, theta, phi, alpha=np.pi/4, steps=180, r1=r1, estimate=1)
+#     xys.append([x, y])
+    
+
+# plot_it2(xys, np.array([r1, r3, r5]), individual_points=True)
+
+
+
+# r1 = np.array([-0.5, 0.9, 0])
+# r2 = np.array([0.05, 0.1, -1])
+# theta = theta_angle(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])
+# phi = phi_angle(N(cone_vector(r1[0], r2[0], r1[1], r2[1], r1[2], r2[2])))
+# # print(f'theta = {theta}, phi = {phi}')
+# x, y, ds = x_prime_y_prime_output(1, theta, phi, alpha=np.pi/4, steps=180, r1=r1, estimate=1)
+# print(x, y)
+#plot_it(x, ys=np.array([y]), r1=r1, individual_points=True)
+
+heatmap, extent = get_image(points, 50, 30, 30, 662E3, 50, 0, np.array([-100, 100, -100, 100]), steps=50, ZoomOut=0)
